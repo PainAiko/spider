0063a4d97dfe66fcf17e5221d3e3f6a1459c935b
tokyRT <tokyrt.pro@gmail.com>
2023-06-13 15:44:51 +0300

    fix notification item bg

--- src/shared/components/NotificationItem.tsx
+++ src/shared/components/NotificationItem.tsx
@@ -3,7 +3,7 @@ import userAvatar from "../assets/image/user.jpg";
 
 export default function NotificationItem(){
     return (
-        <div className="flex items-start gap-3 cursor-pointer even:border-t even:border-b border-gray-100 pt-3 pb-5 px-3">
+        <div className="flex items-start gap-3 cursor-pointer even:border-t even:border-b border-gray-100 pt-3 pb-5 px-3 hover:bg-gray-50 transition-all">
             <div className="circle bg-gray-200 rounded-full w-3 h-3 flex-shrink-0 mt-3"></div>
             <img
               src={userAvatar}
